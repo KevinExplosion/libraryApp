5357e28c947ed9716df0b3261a946f69e65186b9
Kevin Mattison and Ramon de la Cruz <virgilioramon.delacruz@gmail.com>
2016-03-02 16:20:47 -0800

    Add author assign functionality.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,13 +9,13 @@ public class App {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
-    get("/", (request, response) -> {
+    get("/", (request, response) -> { //HOME PAGE
       HashMap model = new HashMap();
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    get("/books", (request,response) -> {
+    get("/books", (request,response) -> { //BOOKS PAGE W/ TITLES AND AUTHORS
       HashMap model = new HashMap();
       model.put("titles", Title.all());
       model.put("authors", Author.all());
@@ -23,7 +23,7 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    post("/title", (request, response) -> {
+    post("/title", (request, response) -> { //POSTS TITLES TO BOOKS PAGE
       HashMap model = new HashMap();
       String title = request.queryParams("newBookTitle");
       Title newTitle = new Title(title);
@@ -32,12 +32,34 @@ public class App {
       return null;
     });
 
-    post("/author", (request, response) -> {
+    post("/author", (request, response) -> { //POSTS AUTHORS TO BOOKS PAGE
       HashMap model = new HashMap();
       String author = request.queryParams("newAuthor");
       Author newAuthor = new Author(author);
       newAuthor.save();
       response.redirect("/books");
+      return null;
+    });
+
+
+    get("/title/:id", (request, response) -> {
+      HashMap model = new HashMap();
+      int id = Integer.parseInt(request.params("id"));
+      Title title = Title.find(id);
+      model.put("title", title);
+      model.put("allAuthors", Author.all());
+      model.put("template", "templates/title.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/title/:id", (request, response) -> {
+      HashMap model = new HashMap();
+      int titleId = Integer.parseInt(request.queryParams("titleId"));
+      int authorId = Integer.parseInt(request.queryParams("authorName"));
+      Author author = Author.find(authorId);
+      Title title = Title.find(titleId);
+      title.addAuthor(author);
+      response.redirect("/title/" + titleId);
       return null;
     });
 //
